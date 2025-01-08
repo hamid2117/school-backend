614ab3ba4a4fe768326765b4b7978b6ee272d957
hamid2117 <saim008865@gmail.com>
2025-01-08 10:22:43 +0500

    Enhance user creation process; add role-based permissions and password hashing

--- managers/entities/user/User.manager.js
+++ managers/entities/user/User.manager.js
@@ -6,34 +6,129 @@ module.exports = class User {
     cortex,
     managers,
     validators,
-    mongomodels,
+    oyster,
   } = {}) {
     this.config = config;
     this.cortex = cortex;
+    this.hasher = managers.hasher;
     this.validators = validators;
-    this.mongomodels = mongomodels;
+    this.oyster = oyster;
     this.tokenManager = managers.token;
+    this.responseDispatcher = managers.responseDispatcher;
+    this.shark = managers.shark;
     this.usersCollection = 'users';
     this.userExposed = ['createUser'];
+    this._label = 'user';
+    this.httpExposed = ['createUser'];
   }
+  async _setPermissions({ userId, role }) {
+    const addDirectAccess = ({ nodeId, action }) => {
+      return this.shark.addDirectAccess({
+        userId,
+        nodeId,
+        action,
+      });
+    };
 
-  async createUser({ userName, email, password }) {
-    const user = { userName, email, password };
+    const lookupTable = {
+      admin: async () => {
+        const items = [
+          {
+            nodeId: 'board.school',
+            action: 'read',
+          },
+          {
+            nodeId: 'board.school.class',
+            action: 'update',
+          },
+          {
+            nodeId: 'board.school.class.student',
+            action: 'update',
+          },
+        ];
+        for (const item of items) {
+          await addDirectAccess(item);
+        }
+      },
+      superAdmin: async () => {
+        const items = [
+          {
+            nodeId: 'board.school',
+            action: 'update',
+          },
+          {
+            nodeId: 'board.school.class',
+            action: 'config',
+          },
+          {
+            nodeId: 'board.school.class.student',
+            action: 'config',
+          },
+          {
+            nodeId: 'board.user',
+            action: 'config',
+          },
+        ];
+        for (const item of items) {
+          await addDirectAccess(item);
+        }
+      },
+    };
 
+    if (lookupTable[role]) {
+      await lookupTable[role]();
+    }
+  }
+  async createUser({ userName, email, password, role = 'user', res }) {
     // Data validation
-    let result = await this.validators.user.createUser(user);
+    let result = await this.validators.user.createUser({
+      userName,
+      email,
+      password,
+      role,
+    });
     if (result) return result;
 
     // Creation Logic
-    let createdUser = { userName, email, password };
+    const hashedPassword = await this.hasher.hashPassword(password);
+    const createdUser = await this.oyster.call('add_block', {
+      _id: email,
+      _label: this._label,
+      userName,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    if (createdUser.error) {
+      console.error('Error creating user:', createdUser.error);
+      if (createdUser.error.includes('already exists')) {
+        this.responseDispatcher.dispatch(res, {
+          code: 409,
+          message: 'Email already exists.',
+        });
+        return { selfHandleResponse: true };
+      }
+      this.responseDispatcher.dispatch(res, {
+        code: 500,
+        message: 'Failed to create user',
+      });
+      return { selfHandleResponse: true };
+    }
+
+    // Set permissions based on role
+    await this._setPermissions({ userId: email, role });
+
     let longToken = this.tokenManager.genLongToken({
       userId: createdUser._id,
       userKey: createdUser.key,
     });
 
+    const { password: _password, ...userWithoutPassword } = createdUser;
+
     // Response
     return {
-      user: createdUser,
+      user: userWithoutPassword,
       longToken,
     };
   }
